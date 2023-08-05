e1abb278aacc8038b0d6f58cf2d94543f2fb2054
Liji Mathew <107242364+lijimathew123@users.noreply.github.com>
2023-08-05 17:55:28 +0530

    Delete project_management_system directory

--- project_management_system/project_task/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Employee,Task,Group,Activity
-
-# Register your models here.
-admin.site.register(Employee)
-admin.site.register(Task)
-admin.site.register(Group)
-admin.site.register(Activity)
-
--- project_management_system/project_task/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProjectTaskConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'project_task'
